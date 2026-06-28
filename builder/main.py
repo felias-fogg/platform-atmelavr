04ce4adc44932a5823f0541e07bd882662fdd037
Bernhard Nebel <nebel@hinterm-ziel.de>
2026-06-28 23:48:05 +0200

    5.3.4

--- builder/main.py
+++ builder/main.py
@@ -284,8 +284,6 @@ if env.PioPlatform().get_package_dir("tool-pyavrocd"):
     pyavrocd_dw_disable = pyavrocd_path + " -m all -d ${BOARD_MCU} --debugwire disable"
 else:
     pyavrocd_dw_disable = "echo 'pyavrocd not yet installed'; exit 1"
-pyavrocd_path = join(env.PioPlatform().get_package_dir("tool-pyavrocd"), "pyavrocd")
-pyavrocd_dw_disable = pyavrocd_path + " -m all -d ${BOARD_MCU} --debugwire disable"
 env.AddPlatformTarget("dwoff", None,
 env.VerboseAction(pyavrocd_dw_disable, "Disabling debugWIRE"),
 "Disable debugWIRE")
